48f3e404126eed857ea8b4b54b246571a7f76400
prabha79 <prabhakar_r@hcl.com>
2019-10-29 12:34:48 +0530

    Initial files

---
+++ MemUsage/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MemUsage
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Loop();
+        }
+
+        private static void Loop()
+        {
+            LoopTillEsc();
+            LoopTillQ();
+        }
+
+        private static void LoopTillEsc()
+        {
+            //var mem = StaticAlloc.AllocMem();
+            StaticAlloc.AllocMemLocal();
+        }
+
+        private static void LoopTillQ()
+        {
+            Console.WriteLine("Press Q to quit");
+            do
+            {
+                while (!Console.KeyAvailable)
+                {
+                   int []arr = new int[1024]; // Do something
+                }
+            } while (Console.ReadKey(true).Key != ConsoleKey.Q);
+        }
+    }
+}
---
+++ MemUsage/StaticAlloc.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MemUsage
+{
+    public class MemType
+    {
+        private static int[] i = new int[1024 * 1024 * 4];
+        
+    }
+   public class StaticAlloc
+    {
+        static List<MemType> memTypes = new List<MemType>();
+        public static List<MemType> AllocMem()
+        {
+            
+            Console.WriteLine("Press ESC to stop Mem allocation");
+            do
+            {
+                while (!Console.KeyAvailable)
+                {
+                    MemType memType = new MemType();
+                    memTypes.Add(memType);
+                }
+            } while (Console.ReadKey(true).Key != ConsoleKey.Escape);
+
+            return memTypes;
+
+        }
+
+        public static void AllocMemLocal()
+        {
+            List<MemType> memTypes = new List<MemType>();
+            MemType memType;
+            Console.WriteLine("Press ESC to stop Mem allocation");
+            do
+            {
+                while (!Console.KeyAvailable)
+                {
+                    memType = new MemType();
+                    memTypes.Add(memType);
+                }
+            } while (Console.ReadKey(true).Key != ConsoleKey.Escape);
+
+            //return memTypes;
+
+        }
+
+
+    }
+}
